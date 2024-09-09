ad96bcbd7d4620bdb903404cf972749f214d3726
Sandis <smanssandis@gmail.com>
2024-09-10 01:04:34 +0300

    Add project files.

---
+++ C#_1.pr.darbs_DVDZ/Program.cs
@@ -0,0 +1,131 @@
+﻿
+// 1. Uzdevums
+double a = 5;
+double b = 4;
+
+double multiplication = a * b;
+double subtraction = a - b;
+double sum = a + b;
+double division = a / b;
+
+Console.WriteLine("1. Uzdevums");
+Console.WriteLine("---------------------------");
+Console.WriteLine("Skaitlis A = " + a);
+Console.WriteLine("Skaitlis B = " + b);
+Console.WriteLine("Operācijas reizināt darbība: " + a + "*" + b + "= " + multiplication);
+Console.WriteLine("Operācijas atņemt darbība: " + a + "-" + b + "= " + subtraction);
+Console.WriteLine("Operācijas summa darbība: " + a + "+" + b + "= " + sum);
+Console.WriteLine("Operācijas dalīt darbība: " + a + "/" + b + "= " + division);
+Console.WriteLine();
+Console.WriteLine();
+
+
+// 2. Uzdevums
+double pi = 3.1415926;
+double radius = 2.4;
+
+double circumference = 2 * pi * radius;
+double circleArea = pi * radius * radius;
+double sphereArea = 4 * pi * radius * radius;
+double sphereVolume = (4 * pi * radius * radius * radius) / 3;
+
+Console.WriteLine("2. Uzdevums");
+Console.WriteLine("---------------------------");
+Console.WriteLine("Rādiuss R = " + radius);
+Console.WriteLine("Riņķa Līnijas Garums = " + circumference);
+Console.WriteLine("Riņka Laukums = " + circleArea);
+Console.WriteLine("Lodes Laukums = " + sphereArea);
+Console.WriteLine("Lodes Tilpums = " + sphereVolume);
+Console.WriteLine();
+Console.WriteLine();
+
+
+// 3. Uzdevums
+Console.WriteLine("3. Uzdevums");
+Console.WriteLine("---------------------------");
+Console.WriteLine("Ievadiet x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ievadiet y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("x + y = " + (x + y));
+Console.WriteLine("x - y = " + (x - y));
+Console.WriteLine("x * y = " + (x * y));
+Console.WriteLine("x / y = " + (x / y));
+Console.WriteLine("x % y = " + (x % y));
+Console.WriteLine("x / y = " + ((double)x / y));
+Console.WriteLine("x*x + y*y = " + (x * x + y * y));
+Console.WriteLine();
+Console.WriteLine();
+
+
+// 4. Uzdevums
+Console.WriteLine("4. Uzdevums");
+Console.WriteLine("---------------------------");
+Console.WriteLine("\t1\t2\t3\t4\t5\t6\t7\t8\t9\t10");
+Console.WriteLine("----------------------------------------------------------------------------------");
+Console.WriteLine("1 |\t" + 1 * 1 + "\t" + 1 * 2 + "\t" + 1 * 3 + "\t" + 1 * 4 + "\t" + 1 * 5 + "\t" + 1 * 6 + "\t" + 1 * 7 + "\t" + 1 * 8 + "\t" + 1 * 9 + "\t" + 1 * 10);
+Console.WriteLine("2 |\t" + 2 * 1 + "\t" + 2 * 2 + "\t" + 2 * 3 + "\t" + 2 * 4 + "\t" + 2 * 5 + "\t" + 2 * 6 + "\t" + 2 * 7 + "\t" + 2 * 8 + "\t" + 2 * 9 + "\t" + 2 * 10);
+Console.WriteLine("3 |\t" + 3 * 1 + "\t" + 3 * 2 + "\t" + 3 * 3 + "\t" + 3 * 4 + "\t" + 3 * 5 + "\t" + 3 * 6 + "\t" + 3 * 7 + "\t" + 3 * 8 + "\t" + 3 * 9 + "\t" + 3 * 10);
+Console.WriteLine("4 |\t" + 4 * 1 + "\t" + 4 * 2 + "\t" + 4 * 3 + "\t" + 4 * 4 + "\t" + 4 * 5 + "\t" + 4 * 6 + "\t" + 4 * 7 + "\t" + 4 * 8 + "\t" + 4 * 9 + "\t" + 4 * 10);
+Console.WriteLine("5 |\t" + 5 * 1 + "\t" + 5 * 2 + "\t" + 5 * 3 + "\t" + 5 * 4 + "\t" + 5 * 5 + "\t" + 5 * 6 + "\t" + 5 * 7 + "\t" + 5 * 8 + "\t" + 5 * 9 + "\t" + 5 * 10);
+Console.WriteLine("6 |\t" + 6 * 1 + "\t" + 6 * 2 + "\t" + 6 * 3 + "\t" + 6 * 4 + "\t" + 6 * 5 + "\t" + 6 * 6 + "\t" + 6 * 7 + "\t" + 6 * 8 + "\t" + 6 * 9 + "\t" + 6 * 10);
+Console.WriteLine("7 |\t" + 7 * 1 + "\t" + 7 * 2 + "\t" + 7 * 3 + "\t" + 7 * 4 + "\t" + 7 * 5 + "\t" + 7 * 6 + "\t" + 7 * 7 + "\t" + 7 * 8 + "\t" + 7 * 9 + "\t" + 7 * 10);
+Console.WriteLine("8 |\t" + 8 * 1 + "\t" + 8 * 2 + "\t" + 8 * 3 + "\t" + 8 * 4 + "\t" + 8 * 5 + "\t" + 8 * 6 + "\t" + 8 * 7 + "\t" + 8 * 8 + "\t" + 8 * 9 + "\t" + 8 * 10);
+Console.WriteLine("9 |\t" + 9 * 1 + "\t" + 9 * 2 + "\t" + 9 * 3 + "\t" + 9 * 4 + "\t" + 9 * 5 + "\t" + 9 * 6 + "\t" + 9 * 7 + "\t" + 9 * 8 + "\t" + 9 * 9 + "\t" + 9 * 10);
+Console.WriteLine("10|\t" + 10 * 1 + "\t" + 10 * 2 + "\t" + 10 * 3 + "\t" + 10 * 4 + "\t" + 10 * 5 + "\t" + 10 * 6 + "\t" + 10 * 7 + "\t" + 10 * 8 + "\t" + 10 * 9 + "\t" + 10 * 10);
+
+
+// 5. Uzdevums
+Console.WriteLine("5. Uzdevums");
+Console.WriteLine("---------------------------");
+
+var originalForegroundColor = Console.ForegroundColor;
+var originalBackgroundColor = Console.BackgroundColor;
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write(".d88888b  \r\n88.    \"' \r\n`Y88888b. \r\n      `8b \r\nd8'   .8P \r\n Y88888P");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.BackgroundColor = ConsoleColor.White;
+Console.Write("\r\n         \r\n.d8888b. \r\n88'  `88 \r\n88.  .88 \r\n`88888P8 \r\n         \r\n         ");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.BackgroundColor = ConsoleColor.DarkGray;
+Console.Write("\r\n         \r\n88d888b. \r\n88'  `88 \r\n88    88 \r\ndP    dP \r\n         \r\n         \n");
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.BackgroundColor = ConsoleColor.Red;
+Console.Write("      dP \r\n      88 \r\n.d888b88 \r\n88'  `88 \r\n88.  .88 \r\n`88888P8 \r\n         \r\n");
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+Console.BackgroundColor = ConsoleColor.Cyan;
+Console.Write("oo \r\n   \r\ndP \r\n88 \r\n88 \r\ndP \r\n   \r\n   ");
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.Write("         \r\n         \r\n.d8888b. \r\nY8ooooo. \r\n      88 \r\n`88888P'");
+
+Console.ForegroundColor = originalForegroundColor;
+Console.BackgroundColor = originalBackgroundColor;
+
+
+
+// 6. Uzdevums
+
+Console.WriteLine("6. Uzdevums");
+Console.WriteLine("---------------------------");
+
+Console.WriteLine("Ievadiet skaitli A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ievadiet skaitli B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ievadiet skaitli C: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(a + "^2 - " + b + "^2 = (" +a + "-" + b + ")*(" + a +"+" + b +")");
+Console.WriteLine((a * a) +"-"+ (b*b) + " = " + "(" + (a-b) + ")*(" + (a + b) + ")");
+Console.WriteLine((a * a) - (b * b) + " = " + (a - b) * (a + b));
